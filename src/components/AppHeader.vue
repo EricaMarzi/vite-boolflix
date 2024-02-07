02ec3f3ba4ba522c06ecbf9732285a7c7d305a12
EricaMarzi <erica.marzi.1993@gmail.com>
2024-02-07 16:16:34 +0100

    Search Form component

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,8 +1,22 @@
 <script>
-import SearchBar from './SearchBar.vue'
+import axios from 'axios';
+import SearchForm from './SearchForm.vue';
+const movieEndpoint = 'https://api.themoviedb.org/3/search/movie?api_key=58af0113a84f8b3226f413927cf278f8&language=it-IT';
+import { store } from '../assets/data/store';
 export default {
     name: 'AppHeader',
-    components: { SearchBar }
+    components: { SearchForm },
+    methods: {
+        searchFilms(searchText) {
+            console.log('cerca: ', searchText)
+            const searchMovieEndpoint = `${movieEndpoint}&query=${searchText}`;
+            console.log(searchMovieEndpoint)
+            // axios.get(searchMovieEndpoint).then(res => {
+            //     this.store = res.data.result
+            // })
+            // console.log(res.data.result)
+        }
+    }
 }
 </script>
 
@@ -12,7 +26,7 @@ export default {
             Logo
         </div>
         <div>
-            <SearchBar />
+            <SearchForm @submit-search="searchFilms" />
         </div>
     </header>
 </template>
---
+++ src/components/SearchForm.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: 'SearchForm',
+    data: () => ({
+        searchText: ''
+    }),
+    emits: ['submit-search']
+}
+</script>
+
+<template>
+    <!--@submit.prevent-->
+    <form @submit.prevent="$emit('submit-search', searchText)">
+        <input type="text" placeholder="Cerca Film o Serie" v-model.trim="searchText">
+        <button>Cerca</button>
+    </form>
+</template>
+
+<style lang="scss"></style>
