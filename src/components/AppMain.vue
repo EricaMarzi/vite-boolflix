dbd7f2ffdddb5da2929d191e2f5ed517d299ebaa
EricaMarzi <erica.marzi.1993@gmail.com>
2024-02-08 12:18:41 +0100

    film+serie in page

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,18 +4,28 @@ export default {
     name: 'AppMain',
     data: () => ({
         store
-    })
+    }),
 }
 console.log({ store })
 </script>
 
 <template>
     <main>
+        <h2>Film</h2>
         <ul v-for="movie in store.movies" :key="movie.id">
             <li>{{ movie.title }}</li>
             <li>{{ movie.original_title }}</li>
             <li>{{ movie.original_language }}</li>
             <li>{{ movie.vote_average }}</li>
+        </ul>
+
+
+        <h2>Serie TV</h2>
+        <ul v-for="serie in store.series" :key="serie.id">
+            <li>{{ serie.name }}</li>
+            <li>{{ serie.original_name }}</li>
+            <li>{{ serie.original_language }}</li>
+            <li>{{ serie.vote_average }}</li>
         </ul>
     </main>
 </template>
