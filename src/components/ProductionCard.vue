cafae2d90cd3cca06412d727688874f99af26252
EricaMarzi <erica.marzi.1993@gmail.com>
2024-02-08 12:39:41 +0100

    add computed lang

--- src/components/ProductionCard.vue
+++ src/components/ProductionCard.vue
@@ -11,12 +11,15 @@ export default {
         originalTitle() {
             return this.production.original_title || this.production.original_name
         },
+        lang() {
+            return this.production.original_language
+        },
         hasFlag() {
             const flags = ['en', 'it'];
-            return flags.includes(this.production.original_language)
+            return flags.includes(this.lang)
         },
         flagUrl() {
-            const url = new URL(`../assets/img/${this.production.original_language}.png`, import.meta.url);
+            const url = new URL(`../assets/img/${this.lang}.png`, import.meta.url);
             return url.href;
         }
     }
@@ -28,8 +31,8 @@ export default {
         <li>{{ title }}</li>
         <li>{{ originalTitle }}</li>
         <li>
-            <img v-if="hasFlag" :src="flagUrl" alt="">
-            <span v-else>{{ production.original_language }}</span>
+            <img v-if="hasFlag" :src="flagUrl" :alt="lang">
+            <span v-else>{{ lang }}</span>
         </li>
         <li>{{ production.vote_average }}</li>
     </ul>
