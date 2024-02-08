e87caa240a031fd6d16978a431764ec791169cc0
EricaMarzi <erica.marzi.1993@gmail.com>
2024-02-08 12:37:34 +0100

    flag it/en

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -17,22 +17,10 @@ console.log({ store })
     <main>
         <h2>Film</h2>
         <ProductionCard v-for="movie in store.movies" :key="movie.id" :production="movie" />
-        <!-- <ul v-for="movie in store.movies" :key="movie.id">
-            <li>{{ movie.title }}</li>
-            <li>{{ movie.original_title }}</li>
-            <li>{{ movie.original_language }}</li>
-            <li>{{ movie.vote_average }}</li>
-        </ul> -->
 
 
         <h2>Serie TV</h2>
         <ProductionCard v-for="serie in store.series" :key="serie.id" :production="serie" />
-        <!-- <ul v-for="serie in store.series" :key="serie.id">
-            <li>{{ serie.name }}</li>
-            <li>{{ serie.original_name }}</li>
-            <li>{{ serie.original_language }}</li>
-            <li>{{ serie.vote_average }}</li>
-        </ul> -->
     </main>
 </template>
 
--- src/components/ProductionCard.vue
+++ src/components/ProductionCard.vue
@@ -3,15 +3,34 @@ export default {
     name: 'ProcutionCard',
     props: {
         production: Object
+    },
+    computed: {
+        title() {
+            return this.production.title || this.production.name
+        },
+        originalTitle() {
+            return this.production.original_title || this.production.original_name
+        },
+        hasFlag() {
+            const flags = ['en', 'it'];
+            return flags.includes(this.production.original_language)
+        },
+        flagUrl() {
+            const url = new URL(`../assets/img/${this.production.original_language}.png`, import.meta.url);
+            return url.href;
+        }
     }
 }
 </script>
 
 <template>
     <ul>
-        <li>{{ production.title || production.name }}</li>
-        <li>{{ production.original_title || production.original_name }}</li>
-        <li>{{ production.original_language }}</li>
+        <li>{{ title }}</li>
+        <li>{{ originalTitle }}</li>
+        <li>
+            <img v-if="hasFlag" :src="flagUrl" alt="">
+            <span v-else>{{ production.original_language }}</span>
+        </li>
         <li>{{ production.vote_average }}</li>
     </ul>
 </template>
